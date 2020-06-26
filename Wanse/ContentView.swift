52a59cae36380b1a951581da094dafbb5f91f2e6
gaoqi <380944602@qq.com>
2020-06-26 10:08:11 +0800

    Initial Commit

---
+++ Wanse/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Wanse
+//
+//  Created by gaoqi on 2020/6/26.
+//  Copyright © 2020 wanse. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
